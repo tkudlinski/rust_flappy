b2ffceae6f1dc907cf19111c16a7f6c1b5c15083
tkudlinski <tomasz.kudlinski@gmail.com>
2022-09-05 22:09:23 +0200

    split into modules

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,15 @@
-use bracket_lib::prelude::*;
+mod constants;
+mod obstacle;
+mod player;
 
-const SCREEN_WIDTH: i32 = 80;
-const SCREEN_HEIGHT: i32 = 50;
-const FRAME_DURATION: f32 = 75.0;
-
-struct Obstacle {
-    x: i32,
-    gap_y: i32,
-    size: i32,
+mod prelude {
+    pub use crate::constants::*;
+    pub use crate::obstacle::*;
+    pub use crate::player::*;
+    pub use bracket_lib::prelude::*;
 }
 
-impl Obstacle {
-    fn new(x: i32, score: i32) -> Self {
-        let mut random = RandomNumberGenerator::new();
-        Obstacle {
-            x,
-            gap_y: random.range(10, 20),
-            size: i32::max(2, 20 - score),
-        }
-    }
-    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
-        let screen_x = self.x - player_x;
-        let half_size = self.size / 2;
-
-        for y in 0..self.gap_y - half_size {
-            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'))
-        }
-
-        for y in self.gap_y + half_size..SCREEN_HEIGHT {
-            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'))
-        }
-    }
-
-    fn hit_obstacle(&self, player: &Player) -> bool {
-        let half_size = self.size / 2;
-        let does_x_match = player.x == self.x;
-        let player_above_gap = player.y < self.gap_y - half_size;
-        let player_below_gap = player.y > self.gap_y + half_size;
-        does_x_match && (player_above_gap || player_below_gap)
-    }
-}
-
-struct Player {
-    x: i32,
-    y: i32,
-    velocity: f32,
-}
-
-impl Player {
-    fn new(x: i32, y: i32) -> Self {
-        Player {
-            x,
-            y,
-            velocity: 0.0,
-        }
-    }
-    fn render(&mut self, ctx: &mut BTerm) {
-        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
-    }
-    fn gravity_and_move(&mut self) {
-        if self.velocity < 2.0 {
-            self.velocity += 2.0;
-        }
-
-        self.y += self.velocity as i32;
-        self.x += 1;
-        if self.y < 0 {
-            self.y = 0;
-        }
-    }
-    fn flap(&mut self) {
-        self.velocity = -4.0;
-    }
-}
+use prelude::*;
 
 struct State {
     player: Player,
@@ -167,8 +104,6 @@ enum GameMode {
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        //  ctx.cls();
-        //  ctx.print(1, 1, "Hello Dragon");
         match self.mode {
             GameMode::Menu => self.main_menu(ctx),
             GameMode::Playing => self.play(ctx),
@@ -177,10 +112,8 @@ impl GameState for State {
     }
 }
 fn main() -> BError {
-    println!("Hello, world!");
-
     let context = BTermBuilder::simple80x50()
-        .with_title("Falppy Dragon")
+        .with_title("Flappy Dragon")
         .build()?;
 
     main_loop(context, State::new())
